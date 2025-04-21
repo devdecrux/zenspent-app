4fd7a25a987500117c96be7b39151d773b009730
decrux <milen.ovalchev@gmail.com>
2025-04-21 16:50:36 +0300

    init

---
+++ zenspent/src/main/java/com/decrux/zenspent/ZenSpentApplication.java
@@ -0,0 +1,13 @@
+package com.decrux.zenspent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZenSpentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZenSpentApplication.class, args);
+	}
+
+}
---
+++ zenspent/src/test/java/com/decrux/zenspent/ZenSpentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.decrux.zenspent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZenSpentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
