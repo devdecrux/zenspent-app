f0dba85590942adecd581968ba17738a703f1adb
Milen Valchev <milen.ovalchev@gmail.com>
2025-07-16 20:47:43 +0300

    - add Dark/Light mode - add toggle button for choosing between theme mode

--- zenspent-ui-v2/src/components/ZSidebar.vue
+++ zenspent-ui-v2/src/components/ZSidebar.vue
@@ -21,6 +21,9 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Button } from '@/components/ui/button'
+import { Icon } from '@iconify/vue'
+import { useColorMode } from '@vueuse/core'
 
 const routes = [
   { name: 'Dashboard', path: '/dashboard', icon: Home },
@@ -32,6 +35,8 @@ const routes = [
 const props = withDefaults(defineProps<SidebarProps>(), {
   collapsible: 'icon',
 })
+
+const mode = useColorMode()
 </script>
 
 <template>
@@ -81,6 +86,36 @@ const props = withDefaults(defineProps<SidebarProps>(), {
     <!--        </SidebarMenu>-->
     <!--      </SidebarGroupContent>-->
     <!--    </SidebarGroup>-->
+
+    <SidebarGroup>
+      <SidebarGroupContent>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <DropdownMenu>
+              <DropdownMenuTrigger as-child>
+                <Button>
+                  <Icon
+                    icon="radix-icons:moon"
+                    class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+                  />
+                  <Icon
+                    icon="radix-icons:sun"
+                    class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+                  />
+                  <span class="sr-only">Toggle theme</span>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuItem v-on:click="mode = 'light'"> Light</DropdownMenuItem>
+                <DropdownMenuItem v-on:click="mode = 'dark'"> Dark</DropdownMenuItem>
+                <DropdownMenuItem v-on:click="mode = 'auto'"> System</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
+
     <Separator />
 
     <SidebarFooter>
