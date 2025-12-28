b952b6f5186e98fe4641e7b669866404b78dc244
Milen Valchev <milen.ovalchev@gmail.com>
2025-12-28 15:48:37 +0200

    - Add missed Theme Menu file

---
+++ zenspent-ui-v2/src/components/ThemeMenu.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { DropdownMenuLabel, DropdownMenuRadioGroup, DropdownMenuRadioItem } from '@/components/ui/dropdown-menu'
+import { Monitor, Moon, Sun } from 'lucide-vue-next'
+import { useColorMode } from '@vueuse/core'
+
+const mode = useColorMode()
+</script>
+
+<template>
+  <DropdownMenuLabel>Theme</DropdownMenuLabel>
+  <DropdownMenuRadioGroup v-model="mode">
+    <DropdownMenuRadioItem value="light">
+      <Sun class="size-4" />
+      Light
+    </DropdownMenuRadioItem>
+    <DropdownMenuRadioItem value="dark">
+      <Moon class="size-4" />
+      Dark
+    </DropdownMenuRadioItem>
+    <DropdownMenuRadioItem value="auto">
+      <Monitor class="size-4" />
+      System
+    </DropdownMenuRadioItem>
+  </DropdownMenuRadioGroup>
+</template>
